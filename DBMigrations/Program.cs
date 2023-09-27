b32a1411d7d88ab9745e30e3911b1b36bc419947
Yugandhar Reddy <Yugandhar.Reddy@halliburton.com>
2023-09-27 14:54:49 +0530

    Add project files.

---
+++ DBMigrations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DBMigrations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DBMigrations/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBMigrations
+{
+    public class SchoolContext : DbContext
+    {
+        //entities
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=127.0.0.1,1433;Database=SchoolDB;Trusted_Connection=True;");
+        }
+    }
+}
